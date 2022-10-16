ec49c5923ef5ae9bffd6c8b03bf6f083782e1307
kusakabe <kusakabe.takuya@alleeks.com>
2022-10-16 20:02:52 +0900

    feat add icon

---
+++ src/components/Icon.tsx
@@ -0,0 +1,16 @@
+export const UnfoldMoreIcon = () => {
+  return (
+    <svg
+      width="12"
+      height="23"
+      viewBox="0 0 12 23"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.625 22.375L0 16.75L1.375 15.375L5.625 19.625L9.875 15.375L11.25 16.75L5.625 22.375ZM1.375 7L0 5.625L5.625 0L11.25 5.625L9.875 7L5.625 2.75L1.375 7Z"
+        fill="black"
+      />
+    </svg>
+  );
+};
